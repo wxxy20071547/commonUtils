fca34e770519a32921e31451f4e0ac48404a5dc4
zhengkun <zk@kuaihuoyun.com>
2018-04-11 16:16:33 +0800

    sumbit

--- src/main/java/com/kevin/common/Test.java
+++ src/main/java/com/kevin/common/Test.java
@@ -2,6 +2,8 @@ package com.kevin.common;
 
 
 import com.kevin.common.domain.People;
+import com.kevin.common.rpc.service.HelloService;
+import com.kevin.common.rpc.service.impl.AbstractHelloService;
 import org.nutz.lang.Mirror;
 
 /**
@@ -10,6 +12,13 @@ import org.nutz.lang.Mirror;
 public class Test {
 
     public static void main(String[] args) {
+        HelloService hs = new AbstractHelloService() {
+            @Override
+            public String hello(String name) {
+                return "this is a new " +name;
+            }
+        };
+        System.out.println(hs.hello("very good"));
         People people = new People();
         Mirror peopleMirror = Mirror.me(people);
         peopleMirror.invoke(people,"setName","tom");
---
+++ src/main/java/com/kevin/common/rpc/service/impl/AbstractHelloService.java
@@ -0,0 +1,13 @@
+package com.kevin.common.rpc.service.impl;
+
+import com.kevin.common.rpc.service.HelloService;
+
+/**
+ * Created by kevin on 2018/4/11.
+ */
+public abstract class AbstractHelloService implements HelloService {
+
+    public String hello(String name){
+        return null;
+    }
+}
