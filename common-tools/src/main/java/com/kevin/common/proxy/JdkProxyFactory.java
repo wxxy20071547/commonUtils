b0ad1aef9b12cb2d587bb1dd5ccff7f4ee0e2c0e
kevin <zk@kuaihuoyun.com>
2018-05-13 18:04:05 +0800

    submit

--- common-tools/src/main/java/com/kevin/common/proxy/JdkProxyFactory.java
+++ common-tools/src/main/java/com/kevin/common/proxy/JdkProxyFactory.java
@@ -16,6 +16,14 @@ public class JdkProxyFactory extends AbstractProxyFactory implements InvocationH
                ,this.targetObject.getClass().getInterfaces(),this);
     }
 
+    /**
+     *
+     * @param proxy 指代理的类 如：com.kevin.common.proxy.impl.PeopleServiceImpl
+     * @param method 指代理类的方法 如：public abstract void com.kevin.common.proxy.service.PeopleService.say(java.lang.String)
+     * @param args 指参数 如：jdk proxy is ok
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         return invoke(method,this.targetObject,args);
