5673bbcd44b806caaed821ece0e5ade156fda12a
kevin <zk@kuaihuoyun.com>
2018-05-13 18:16:16 +0800

    submit

--- common-tools/src/main/java/com/kevin/common/proxy/ProxyTest.java
+++ common-tools/src/main/java/com/kevin/common/proxy/ProxyTest.java
@@ -2,21 +2,18 @@ package com.kevin.common.proxy;
 
 import com.kevin.common.proxy.impl.PeopleServiceImpl;
 import com.kevin.common.proxy.service.PeopleService;
+import org.junit.jupiter.api.Test;
 
 public class ProxyTest {
-    public static void main(String[] args) {
-        testJdkProxy();
-        testCglibProxy();
-    }
-
-    public static void testJdkProxy(){
+    @Test
+    public  void testJdkProxy(){
         JdkProxyFactory proxyFactory = new JdkProxyFactory();
         PeopleService peopleService = (PeopleService) proxyFactory.createProxyIntance(new PeopleServiceImpl());
         peopleService.say("jdk proxy is ok");
     }
 
-
-    public static void testCglibProxy(){
+    @Test
+    public  void testCglibProxy(){
         CglibProxyFactory proxyFactory = new CglibProxyFactory();
         PeopleServiceImpl peopleService = (PeopleServiceImpl) proxyFactory.createProxyIntance(new PeopleServiceImpl());
         peopleService.say("cglib proxy is ok");
