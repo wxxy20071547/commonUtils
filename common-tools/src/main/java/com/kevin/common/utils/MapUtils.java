0aa15b5fc620a0323eed499904388b68151ed94b
zhengkun <zk@kuaihuoyun.com>
2018-08-15 20:33:13 +0800

    fix code

--- common-tools/src/main/java/com/kevin/common/utils/MapUtils.java
+++ common-tools/src/main/java/com/kevin/common/utils/MapUtils.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Maps;
 
 import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by kevin on 2018/6/11.
@@ -12,23 +13,19 @@ public class MapUtils {
 
     public static Map<String, Object> beanToMap(Object obj) {
         Map<String, Object> reMap = Maps.newHashMap();
-        if (obj == null)
+        if (Objects.isNull(obj))
             return null;
         Field[] fields = obj.getClass().getDeclaredFields();
-        try {
-            for (int i = 0; i < fields.length; i++) {
-                try {
-                    String fileName = fields[i].getName();
-                    Field f = obj.getClass().getDeclaredField(fileName);
-                    f.setAccessible(true);
-                    Object o = f.get(obj);
-                    reMap.put(fileName, o);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        for (Field field : fields) {
+            try {
+                String fileName = field.getName();
+                Field f = obj.getClass().getDeclaredField(fileName);
+                f.setAccessible(true);
+                Object o = f.get(obj);
+                reMap.put(fileName, o);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (SecurityException e) {
-            e.printStackTrace();
         }
         return reMap;
     }
