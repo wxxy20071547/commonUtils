18fdb480dff0d702ba8908c5016254ee51c9a77a
zhengkun <zk@kuaihuoyun.com>
2018-09-20 15:00:05 +0800

    fix code

--- common-tools/src/main/java/com/kevin/common/Test.java
+++ common-tools/src/main/java/com/kevin/common/Test.java
@@ -20,18 +20,35 @@ public class Test {
     public static void main(String[] args) {
         People people = new People();
         Mirror peopleMirror = Mirror.me(people);
-        peopleMirror.invoke(people,"setName","tom");
-        System.out.println(peopleMirror.invoke(people,"getName"));
+        peopleMirror.invoke(people, "setName", "tom");
+        System.out.println(peopleMirror.invoke(people, "getName"));
 
 
         //按访问顺序
-        Map linkMap = new LinkedHashMap(16,0.75f,true);
+        Map linkMap = new LinkedHashMap(16, 0.75f, true);
         for (int i = 0; i < 10; i++) {
-            linkMap.put(String.valueOf(i),i);
+            linkMap.put(String.valueOf(i), i);
         }
         linkMap.get("4");
         linkMap.get("1");
         System.out.println(linkMap);
+        try {
+            chufa();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
+
+    public static void chufa() {
+        try {
+            int i = 1/0;
+        } catch (Exception e){
+
+        }finally {
+            System.out.println("finally");
+        }
+    }
+
+
 }
