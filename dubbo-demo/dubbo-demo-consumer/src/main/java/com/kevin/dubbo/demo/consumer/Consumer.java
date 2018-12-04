2c39409ed9dc699d884573088505ddb61aa20af1
zhengkun <zk@kuaihuoyun.com>
2018-12-04 13:52:38 +0800

    fix code

--- dubbo-demo/dubbo-demo-consumer/src/main/java/com/kevin/dubbo/demo/consumer/Consumer.java
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/com/kevin/dubbo/demo/consumer/Consumer.java
@@ -1,11 +1,20 @@
 package com.kevin.dubbo.demo.consumer;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.kevin.dubbo.demo.DemoService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Consumer {
 
     public static void main(String[] args) {
+        initFlowRules();
         //Prevent to get IPV6 address,this way only work in debug mode
         //But you can pass use -Djava.net.preferIPv4Stack=true,then it work well whether in debug mode or not
         System.setProperty("java.net.preferIPv4Stack", "true");
@@ -14,17 +23,33 @@ public class Consumer {
         DemoService demoService = (DemoService) context.getBean("demoService"); // get remote service proxy
 
         while (true) {
+            Entry entry = null;
             try {
-                Thread.sleep(1000);
+                entry = SphU.entry("HelloWorld");
                 String hello = demoService.sayHello("world"); // call remote method
                 System.out.println(hello); // get result
 
             } catch (Throwable throwable) {
                 throwable.printStackTrace();
+            }finally {
+                if (entry != null) {
+                    entry.exit();
+                }
             }
 
 
         }
 
+    }
+
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<FlowRule>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
     }
 }
