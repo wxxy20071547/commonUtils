18f9d922db37c5890cb6bf8461935ff726bf158e
zhengkun <zk@kuaihuoyun.com>
2019-01-26 16:21:23 +0800

    fix code

---
+++ common-tools/src/main/java/excel/ExcelTest.java
@@ -0,0 +1,39 @@
+package excel;
+
+import com.xuxueli.poi.excel.ExcelExportUtil;
+import com.xuxueli.poi.excel.ExcelImportUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by kevin on 2019/1/26.
+ */
+public class ExcelTest {
+    public static void main(String[] args) {
+        /**
+         * Mock数据，Java对象列表
+         */
+        List<ShopDTO> shopDTOList = new ArrayList<ShopDTO>();
+        for (int i = 0; i < 100; i++) {
+            ShopDTO shop = new ShopDTO(true, "商户" + i, (short) i, 1000 + i, 10000 + i, (float) (1000 + i), (double) (10000 + i), new Date());
+            shopDTOList.add(shop);
+        }
+        String filePath = "/Users/kevin/Downloads/demo-kevin-sheet.xls";
+
+        /**
+         * Excel导出：Object 转换为 Excel
+         */
+        ExcelExportUtil.exportToFile(filePath, shopDTOList);
+
+        /**
+         * Excel导入：Excel 转换为 Object
+         */
+        List<Object> list = ExcelImportUtil.importExcel(filePath, ShopDTO.class);
+
+        System.out.println(list);
+
+    }
+
+}
---
+++ common-tools/src/main/java/excel/ShopDTO.java
@@ -0,0 +1,51 @@
+package excel;
+
+import com.xuxueli.poi.excel.annotation.ExcelField;
+import com.xuxueli.poi.excel.annotation.ExcelSheet;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+
+import java.util.Date;
+
+/**
+ * Created by kevin on 2019/1/26.
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@ExcelSheet(name = "商户列表", headColor = HSSFColor.HSSFColorPredefined.LIGHT_GREEN)
+public class ShopDTO {
+
+    @ExcelField(name = "是否VIP商户")
+    private boolean vip;
+
+    @ExcelField(name = "商户名称", align = HorizontalAlignment.CENTER)
+    private String shopName;
+
+    @ExcelField(name = "分店数量")
+    private short branchNum;
+
+    @ExcelField(name = "商户ID")
+    private int shopId;
+
+    @ExcelField(name = "浏览人数")
+    private long visitNum;
+
+    @ExcelField(name = "当月营业额")
+    private float turnover;
+
+    @ExcelField(name = "历史营业额")
+    private double totalTurnover;
+
+    @ExcelField(name = "开店时间", dateformat = "yyyy-MM-dd HH:mm:ss")
+    private Date addTime;
+
+
+}
+
+
