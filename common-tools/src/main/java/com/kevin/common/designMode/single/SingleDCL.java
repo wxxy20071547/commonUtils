fb84565e54fc594f615313a48e3843008989c56a
kevin <zk@kuaihuoyun.com>
2019-09-13 21:03:46 +0800

    submit

---
+++ common-tools/src/main/java/com/kevin/common/designMode/single/SingleDCL.java
@@ -0,0 +1,21 @@
+package com.kevin.common.designMode.single;
+
+public class SingleDCL {
+    private static volatile SingleDCL instance = null;// volatile是为了保证禁止只能重排序
+
+    private SingleDCL() {
+        System.out.println("begin to init class");
+    }
+
+    public static SingleDCL getInstance() {
+        if (instance == null) {
+            synchronized (SingleDCL.class) {
+                if (instance == null) {
+                    instance = new SingleDCL();//这里可能会发生指令重排 加volatile
+                }
+            }
+        }
+        return instance;
+    }
+
+}
