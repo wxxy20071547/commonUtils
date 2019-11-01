5f093486755924c65808b424654f0063b1598ecc
zhengkun <zk4906@qq.com>
2019-11-01 10:33:50 +0800

    commit

---
+++ common-tools/src/main/java/com/kevin/common/utils/PullTest.java
@@ -0,0 +1,11 @@
+package com.kevin.common.utils;
+
+/**
+ * Created by zhengkun
+ * Date: 2019/11/1 10:29 上午
+ */
+public class PullTest {
+    public static void main(String[] args) {
+        System.out.println("new a class file");
+    }
+}
